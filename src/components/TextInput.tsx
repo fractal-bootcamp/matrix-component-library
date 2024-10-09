23555370239aac341ce36bf9afded56c619ef0ff
Rashik Shahjahan <rashikshahjahan@protonmail.com>
2024-10-09 14:24:26 -0400

    styling basic storybook

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,21 +1,29 @@
 interface  TextInputProps {
     disabled: boolean;
+    error:boolean;
     placeholder:string;
     changeHandler?: () => void;
 }
 
 
-
-
 export const TextInput = ({
     disabled = false,
-    placeholder = 'Enter',
+    error = false,
+    placeholder = 'Enter Text',
     ...props
   }: TextInputProps) => {
+
+    const disabledStyle = "border border-black";
+    const enabledStyle  = "text-green-400 border border-black"
+
+    const modeStyle = disabled?disabledStyle:enabledStyle;
+
+    const errorStyle = "border border-black bg-red-400";
+    const successStyle  = "border border-black bg-green-400"
+
+    const validStyle = error?errorStyle:successStyle;
     return (
-        <div>
-            <input type="text" placeholder={placeholder} onChange={()=>{props.changeHandler}} disabled={disabled}/>
-        </div>
+            <input className={`font-mono font-semibold ${modeStyle} ${validStyle}`} type="text" placeholder={disabled?'':placeholder} onChange={()=>{props.changeHandler}} disabled={disabled}/>
     );
   };
 
