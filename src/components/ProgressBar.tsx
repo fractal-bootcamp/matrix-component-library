7e51c7d746d783be776c430ae2cd3d00071a242b
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-09 11:04:53 -0400

    Added base code for ProgresBar

---
+++ src/components/ProgressBar.tsx
@@ -0,0 +1,13 @@
+// Description: a progress bar to indicate the completion percentage of a task
+// Behavior:
+// - Color
+// - Linear / Circular
+// - Customizable progress percentage
+// - Color customization based on progress
+// - Striped and animated stipes option
+
+const ProgressBar = () => {
+	return <div>ProgressBar</div>;
+};
+
+export default ProgressBar;
