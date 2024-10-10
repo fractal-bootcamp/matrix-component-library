cc4172ea3d02de0356a606109af23bbb950b02a6
Daniel Gavidia <dgavidia1@gmail.com>
2024-10-10 15:10:40 -0400

    Drag and drop functionality works

--- src/stories/FileUploader.stories.ts
+++ src/stories/FileUploader.stories.ts
@@ -15,7 +15,7 @@ type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {
 	args: {
-		submitButtonText: "Hello",
+		submitButtonText: "Submit",
 		supportedTypes: ["py", "ts"],
 	},
 };
