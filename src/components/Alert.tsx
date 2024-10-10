1cd772e66017df24f1f03988ce894e8fd921f214
Faisal <faisal@owimer.co>
2024-10-10 17:01:51 -0400

    final commit for publishing

--- src/components/Alert.tsx
+++ src/components/Alert.tsx
@@ -1,41 +1,39 @@
 import React from "react";
 import { useEffect, useState } from "react";
 
-interface  AlertProps {
-    mode:'Warning'|'Info'|'Error'|'Success',
-    message:string,
-    timeout:number
+interface AlertProps {
+  mode: 'Warning' | 'Info' | 'Error' | 'Success',
+  message: string,
+  timeout: number
 }
 
-
 const Alert = ({
-  timeout=2000,
-    ...props
-  }: AlertProps) => {
-    const [hide,setHide] = useState(false);
-    const styleTypes = {
-      'Info':"text-black border-2 border-green-400 ",
-      'Success':"text-green-400  border-2 border-green-400",
-      'Error':"text-red-400 border-2 border-green-400 ",
-      'Warning':"text-yellow-400 border-2 border-green-400",
-    };
-
-    useEffect(()=>{
-      const timeoutId = setTimeout(()=>{
-        setHide(true);
-      },timeout);
-      return () => clearTimeout(timeoutId);
-    },
-  );
-    
-
-    return (
-      <div>
-              {!hide && <div className={`w-64 h-16 font-mono font-semibold animate-slidein ${styleTypes[props.mode]}`} >{props.message}</div>}
-              {hide && <div className={`w-64 h-16 font-mono font-semibold animate-slideout ${styleTypes[props.mode]}`} >{props.message}</div>}
-      </div>
-    );
+  timeout = 2000,
+  ...props
+}: AlertProps) => {
+  const [hide, setHide] = useState(false);
+  const styleTypes = {
+    'Info': "text-black border-2 border-green-400 ",
+    'Success': "text-green-400  border-2 border-green-400",
+    'Error': "text-red-400 border-2 border-green-400 ",
+    'Warning': "text-yellow-400 border-2 border-green-400",
   };
 
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setHide(true);
+    }, timeout);
+    return () => clearTimeout(timeoutId);
+  },
+  );
+
+
+  return (
+    <div>
+      {!hide && <div className={`w-64 h-16 font-mono font-semibold animate-slidein ${styleTypes[props.mode]}`} >{props.message}</div>}
+      {hide && <div className={`w-64 h-16 font-mono font-semibold animate-slideout ${styleTypes[props.mode]}`} >{props.message}</div>}
+    </div>
+  );
+};
+
 export default Alert
-  
