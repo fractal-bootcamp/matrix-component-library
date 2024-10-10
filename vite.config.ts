1c2c7cd3e760c588b275b1ffb6d0bd6b50d240f7
Faisal <faisal@owimer.co>
2024-10-10 17:45:39 -0400

    npm commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		lib: {
+			entry: path.resolve(__dirname, 'src/index.ts'), // Set the index file as the entry point
+			name: 'MatrixComponentLibrary',
+			fileName: (format) => `matrix-component-library.${format}.js`,
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+				},
+			},
+		},
+	},
 });
